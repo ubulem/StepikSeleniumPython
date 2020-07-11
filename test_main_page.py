b71d839292c07b92e7e882340be6c53cbce0498b
ubulem <berkut87@gmail.com>
2020-07-11 13:11:41 +0600

    Add tests for login page

--- test_main_page.py
+++ test_main_page.py
@@ -1,15 +1,26 @@
+import time
+
+from pages.login_page import LoginPage
 from pages.main_page import MainPage
+
+link = "http://selenium1py.pythonanywhere.com/"
 
 
 def test_guest_can_go_to_login_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
     main_page = MainPage(browser, link)
     main_page.open()
     main_page.go_to_login_page()
 
 
 def test_guest_should_see_login_link(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
     page = MainPage(browser, link)
     page.open()
     page.should_be_login_link()
+
+
+def test_guest_should_see_login_page(browser):
+    main_page = MainPage(browser, link)
+    main_page.open()
+    main_page.go_to_login_page()
+    login_page = LoginPage(browser, link)
+    login_page.should_be_login_page()
