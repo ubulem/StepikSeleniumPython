cf09380d9980bd924754ce0036505393dfcc37a6
ubulem <berkut87@gmail.com>
2020-07-12 22:52:13 +0600

    Add negative test

--- pages/product_page.py
+++ pages/product_page.py
@@ -19,3 +19,10 @@ class ProductPage(BasePage):
     def should_be_same_price(self):
         basket_price = self.browser.find_element(*ProductPageLocators.BASKET_PRICE).text
         assert self.product_price == basket_price, f"Basket price is {basket_price}, but expected {self.product_price}"
+
+    def should_not_be_success_message(self):
+        assert self.is_not_element_present(
+            *ProductPageLocators.BASKET_MESSAGE), "Success message is presented, but should not be"
+
+    def should_disappear(self):
+        assert self.is_disappeared(*ProductPageLocators.BASKET_MESSAGE), "Success message not disappeared but should be"
--- test_product_page.py
+++ test_product_page.py
@@ -3,6 +3,7 @@ import pytest
 from pages.product_page import ProductPage
 
 url = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer'
+plain_url = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear'
 
 
 @pytest.mark.parametrize('link', [f"{url}0",
@@ -21,3 +22,25 @@ def test_guest_can_add_product_to_basket(browser, link):
     product_page.add_product_to_basket()
     product_page.should_be_added_to_basket()
     product_page.should_be_same_price()
+
+
+@pytest.mark.xfail
+def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
+    product_page = ProductPage(browser, plain_url)
+    product_page.open()
+    product_page.add_product_to_basket()
+    product_page.should_not_be_success_message()
+
+
+def test_guest_cant_see_success_message(browser):
+    product_page = ProductPage(browser, plain_url)
+    product_page.open()
+    product_page.should_not_be_success_message()
+
+
+@pytest.mark.xfail
+def test_message_disappeared_after_adding_product_to_basket(browser):
+    product_page = ProductPage(browser, plain_url)
+    product_page.open()
+    product_page.add_product_to_basket()
+    product_page.should_disappear()
