ce9eef68c22d621083ad79c669eab0b0669d3c68
ubulem <berkut87@gmail.com>
2020-07-12 22:03:03 +0600

    Add tests for several urls

--- test_product_page.py
+++ test_product_page.py
@@ -1,9 +1,21 @@
+import pytest
+
 from pages.product_page import ProductPage
 
-link = 'http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear';
+url = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer'
 
 
-def test_guest_can_add_product_to_basket(browser):
+@pytest.mark.parametrize('link', [f"{url}0",
+                                  f"{url}1",
+                                  f"{url}2",
+                                  f"{url}3",
+                                  f"{url}4",
+                                  f"{url}5",
+                                  f"{url}6",
+                                  pytest.param(f"{url}7", marks=pytest.mark.xfail),
+                                  f"{url}8",
+                                  f"{url}9"])
+def test_guest_can_add_product_to_basket(browser, link):
     product_page = ProductPage(browser, link)
     product_page.open()
     product_page.add_product_to_basket()
